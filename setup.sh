ab1850a834a839dacf55ffdb9aed68614ba02ecb
W. Clayton Pannell <clayton.pannell@gmail.com>
2019-10-21 01:29:30 -0500

    MPLABX 5.25 + XC8 2.10. Untested

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 VERSION=5.25
+XC8_VERSION=2.10
 INSTALL_DIR=/opt/microchip/mplabx/v${VERSION}
 echo "Installing MPLABX v${VERSION}"
 
@@ -19,17 +20,23 @@ if [ -f "MPLABX-v${VERSION}-linux-installer.tar" ]; then
 else
     echo "Downloading MPLABX v${VERSION} installer..."
     wget -q http://ww1.microchip.com/downloads/en/DeviceDoc/MPLABX-v${VERSION}-linux-installer.tar
+    wget -q http://ww1.microchip.com/downloads/en/DeviceDoc/xc8-v${XC8_VERSION}-full-install-linux-installer.run
 fi
 
 
 # Unpack and install
 tar xf MPLABX-v${VERSION}-linux-installer.tar
+echo "Installing MPLABX v${VERSION}"
 su -c "./MPLABX-v${VERSION}-linux-installer.sh --nolibrarycheck --nox11  -- --ipe 0 --mode unattended --16bitmcu 0 --32bitmcu 0 --othermcu 0 --collectInfo 0"
+echo "Installing XC8 v${XC8_VERSION}"
+chmod u+x xc8-v${XC8_VERSION}-full-install-linux-installer.run
+su -c "./xc8-v${XC8_VERSION}-full-install-linux-installer.run --mode unattended --Add\ to\ PATH 1 --LicenseType FreeMode --netservername $(date | md5sum| cut -c 1-32)"
 
 # strip uneeded packs
 echo "Cleaning up..."
 apt-get purge -y --auto-remove wget
 rm MPLABX-v${VERSION}-linux-installer.*
+rm xc8-v${XC8_VERSION}-full-install-linux-installer.run
 rm ${INSTALL_DIR}/Uninstall_MPLAB_X_IDE*
 rm -rf /opt/microchip/mplabcomm
 rm -rf ${INSTALL_DIR}/packs/arm
